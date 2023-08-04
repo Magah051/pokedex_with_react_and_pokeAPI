1e231230c97557e11cec42aeb995453497be64f1
José Magalhães <joses.magalhaes@hotmail.com>
2023-08-04 16:53:51 -0300

    Ajustes de front-end

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -6,6 +6,7 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { ClassNames } from '@emotion/react';
+import { CardActionArea } from '@mui/material';
 
 export default function PokemonCard({name, image, types}) {
   const typeHandle = () => {
@@ -17,47 +18,41 @@ export default function PokemonCard({name, image, types}) {
 
   return (
     <Card sx={{ maxWidth: 345, marginRight:"1em", marginBottom:"1em" }}>
-      <CardMedia
-        sx={{ height: "auto", paddingTop: "100%" }}
-        image={image}
-        title={name}
-      />
-      <CardContent>
-      <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          sx={{
-            fontSize: "clamp(0.5rem, 2vw, 1rem)",
-            lineHeight: "1.2",
-            overflow: "hidden",
-          }}
-        >
-          {name}
-        </Typography>
-
+      <CardActionArea>
+        <CardMedia
+          sx={{ height: "auto", paddingTop: "100%" }}
+          image={image}
+          title={name}
+        />
+        <CardContent>
         <Typography
-          gutterBottom
-          variant="h6"
-          component="div"
-          sx={{
-            fontSize: "clamp(0.5rem, 2vw, 1rem)",
-            lineHeight: "1.2",
-            overflow: "hidden",
-          }}
-        >
-          {typeHandle()}
-        </Typography>
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={{
+              fontSize: "clamp(0.5rem, 2vw, 1rem)",
+              lineHeight: "1.2",
+              overflow: "hidden",
+            }}
+          >
+            {name}
+          </Typography>
 
-        {/*<Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>*/}
-      </CardContent>
-      {/*<CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-    </CardActions>*/}
+          <Typography gutterBottom variant="caption" component="div">
+            {typeHandle()}
+          </Typography>
+
+          {/*<Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>*/}
+        </CardContent>
+        {/*<CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+      </CardActions>*/}
+    </CardActionArea>
     </Card>
+    
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ export const Home = () => {
 
     const getPokemons = () => {
         var endpoints = []
-        for (var i = 1; i<50;i++){
+        for (var i = 1; i<200;i++){
             endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
         }
     var response = axios.all(endpoints.map((endpoint)=>axios.get(endpoint))).then((res) => setPokemons(res));
