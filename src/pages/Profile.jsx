f0bb4e82cfd9ba0f68437d74b25b8d314165d7db
José Magalhães <joses.magalhaes@hotmail.com>
2023-08-21 15:58:18 -0300

    Inserindo variações de pokémons

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import { Container, Paper, Typography } from "@mui/material";
+import { Container, Divider, Paper, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import PokemonTable from "../components/PokemonTable";
 
@@ -16,6 +16,13 @@ export const Profile = ({pokemonData}) => {
                     <Box display="flex" width="100%">
                         <Box component="img" src={sprites.front_default} width="100%" height="100%"/>
                         <PokemonTable pokemonData={pokemonData}/>
+                    </Box>
+                    <Box width="100%" marginTop={2}>
+                        <Divider>Variações</Divider>
+                        <Box component="img" src={sprites.front_female} width="30%" height="30%"/>
+                        <Box component="img" src={sprites.front_shiny} width="30%" height="30%"/>
+                        <Box component="img" src={sprites.front_shiny_female} width="30%" height="30%"/>
+                        
                     </Box>
                 </Box>
             </Paper>
