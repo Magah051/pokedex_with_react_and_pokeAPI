c5879e59213566afa1b43218dd6c210af42e48dc
José Magalhães <joses.magalhaes@hotmail.com>
2023-08-21 16:42:04 -0300

    Fix bug de rota vazia e ajustes finais do front-end

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import { Chip, Container, Divider, Paper, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import PokemonTable from "../components/PokemonTable";
+import { useNavigate } from "react-router-dom";
 
 export const Profile = ({pokemonData}) => {
-    const {name, sprites, moves} = pokemonData;
+    const {name, sprites, moves} = pokemonData || {};
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!pokemonData){
+            navigate("/");
+        }
+    }, []);
+
+    if (!pokemonData) {
+        return null;
+    }
+
     return (
         <>
         <Navbar hideSearch />
@@ -13,15 +26,26 @@ export const Profile = ({pokemonData}) => {
             <Paper elevation={3}>
                 <Box display="flex" flexDirection="column" alignItems="center" p={5}>
                 <Typography variant="h4">{name}</Typography>
-                    <Box display="flex" alignItems="center" width="50%">
-                        <Box component="img" src={sprites.front_default} width="100%" height="100%"/>
+                    <Box 
+                    display="flex" 
+                    alignItems="center" 
+                    width="70%"
+                    marginBottom="15px"
+                    sx={{
+                        flexDirection: {
+                        xs:"column",
+                        md:"row"
+                    }}}>
+                        <Box component="img" src={sprites.front_default} width="50%" height="100%"/>
                         <PokemonTable pokemonData={pokemonData}/>
                     </Box>
                     <Box width="100%" marginTop={2}>
                         <Divider>Variações</Divider>
-                        <Box component="img" src={sprites.front_female} width="30%" height="30%"/>
-                        <Box component="img" src={sprites.front_shiny} width="30%" height="30%"/>
-                        <Box component="img" src={sprites.front_shiny_female} width="30%" height="30%"/>
+                        <Box display="flex" justifyContent="space-between">
+                            <Box component="img" src={sprites.front_female} width="25%" height="25%"/>
+                            <Box component="img" src={sprites.front_shiny} width="25%" height="25%"/>
+                            <Box component="img" src={sprites.front_shiny_female} width="25%" height="25%"/>
+                        </Box>
                         <Divider>Ataques</Divider>
                         <Box textAlign="center" marginTop="10px">
                             {moves.map((moveData, key) => (
